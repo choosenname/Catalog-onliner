a49dccdc4a6fd3f020d9b3b79af71fbcf6ad0abe
choosenname <vlad@luckashonok.ru>
2024-03-29 20:13:23 +0300

    feat: add reference in swagger

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -41,6 +41,6 @@ class ReviewsController < ApplicationController
   end
 
   def review_params
-    params.require(:review).permit(:title, :body, :rate)
+    params.require(:review).permit(:title, :body, :rate, :product_id)
   end
 end
