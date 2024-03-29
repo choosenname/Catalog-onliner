c859cd7489bd868385b4ff8705f1421e8baa3339
choosenname <vlad@luckashonok.ru>
2024-03-29 14:16:32 +0300

    fix: reformat code

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -54,7 +54,6 @@ RSpec.describe 'categories', type: :request do
   end
 
   path '/categories/{id}' do
-    # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
     get('show category') do
