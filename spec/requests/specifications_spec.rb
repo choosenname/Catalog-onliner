96d546583f77204b67722701b16ce1f32af76407
choosenname <vlad@luckashonok.ru>
2024-03-29 21:42:03 +0300

    feat: add specifications_spec.rb

---
+++ spec/requests/specifications_spec.rb
@@ -0,0 +1,111 @@
+require 'swagger_helper'
+
+RSpec.describe 'specifications', type: :request do
+
+  path '/specifications' do
+
+    get('list specifications') do
+      tags 'Specification'
+      consumes 'application/json'
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create specification') do
+      tags 'Specification'
+      consumes 'application/json'
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/specifications/{id}' do
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show specification') do
+      tags 'Specification'
+      consumes 'application/json'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    patch('update specification') do
+      tags 'Specification'
+      consumes 'application/json'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    put('update specification') do
+      tags 'Specification'
+      consumes 'application/json'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete specification') do
+      tags 'Specification'
+      consumes 'application/json'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
