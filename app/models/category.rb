e977aa600bb5d0b70188d518f6542483124304f4
choosenname <vlad@luckashonok.ru>
2024-03-29 19:34:19 +0300

    feat: add relation between a category and a product

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category
   include Mongoid::Document
   include Mongoid::Timestamps
   field :name, type: String
+  has_many :products, dependent: :destroy
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product
   include Mongoid::Document
   include Mongoid::Timestamps
   field :name, type: String
+  belongs_to :category
 end
