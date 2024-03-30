4ec2d0de41d2a85075c2db8795c45eb9fdfa1a75
choosenname <vlad@luckashonok.ru>
2024-03-30 20:02:56 +0300

    feat: update category id

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category
   include Mongoid::Document
   include Mongoid::Timestamps
+
   field :name, type: String
+  field :_id, type: String, default: -> { name }
+
   has_many :products, dependent: :destroy
 end
