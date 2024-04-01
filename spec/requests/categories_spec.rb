44e8bbf649c80833c2be10a0c97287010b4b849a
Vlad <vlad@luckashonok.ru>
2024-04-01 16:24:28 +0300

    ON-005: add category (#5)
    * feat: init rspec
    
    * feat: init rspec-rails
    
    * feat: add category
    
    * feat: add categories spec
    
    * feat: update swagger.yaml
    
    * fix: reformat code

---
+++ spec/requests/categories_spec.rb
@@ -0,0 +1,127 @@
+require 'swagger_helper'
+
+RSpec.describe 'categories', type: :request do
+
+  path '/categories' do
+
+    get('list categories') do
+      tags 'Category'
+      consumes 'application/json'
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create category') do
+      tags 'Category'
+      consumes 'application/json'
+      parameter name: :category, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string }
+        },
+        required: ['name']
+      }
+
+      response '201', 'category created' do
+        let(:category) { { name: 'foo' } }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:category) { { name: 'foo' } }
+        run_test!
+      end
+
+      response(200, 'successful') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/categories/{id}' do
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show category') do
+      tags 'Category'
+      consumes 'application/json'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    patch('update category') do
+      tags 'Category'
+      consumes 'application/json'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    put('update category') do
+      tags 'Category'
+      consumes 'application/json'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete category') do
+      tags 'Category'
+      consumes 'application/json'
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
